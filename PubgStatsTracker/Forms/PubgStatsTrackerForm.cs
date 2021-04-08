f5c89f7a0cffe25113676ec79b85e01fb0f52b85
FracturedCode <coding1729@gmail.com>
2021-04-08 15:11:25 -0700

    basic 100% stacked chart

--- PubgStatsTracker/Forms/PubgStatsTrackerForm.cs
+++ PubgStatsTracker/Forms/PubgStatsTrackerForm.cs
@@ -1,6 +1,7 @@
 ﻿using MaterialSkin;
 using MaterialSkin.Controls;
 using PubgStatsTracker.BusinessLogic;
+using PubgStatsTracker.Models.Database;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +14,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PubgStatsTracker
 {
@@ -22,8 +24,51 @@ namespace PubgStatsTracker
         {
             MaterialSkinManager.Instance.AddFormToManage(this);
             InitializeComponent();
-            
+
             installButton.Text = AppState.DoesServiceExist ? "Uninstall" : "Install";
+
+            addMapChart();
+        }
+
+        private void addMapChart()
+        {
+            Chart chart = new();
+            chart.Series.Clear();
+            const string chartArea = "chartArea1";
+            const string legend = "legend1";
+            chart.ChartAreas.Add(chartArea);
+            chart.Legends.Add(legend);
+
+            List<MatchHistoryModel> matchHistory = MatchHistoryModel.GetAllHistory();
+            matchHistory.GroupBy(mh => mh.MapId).ToList().ForEach(g =>
+            {
+                string map = g.FirstOrDefault()?.Map.MapName ?? "unknown";
+                Series series = new(map);
+                chart.Series.Add(series);
+                matchHistory.Select(mh => mh.DateTimePlayed.Date).Distinct().ToList().ForEach(d =>
+                    {
+                        chart.Series[map].Points.AddXY(d,
+                            g.Count(mh => mh.DateTimePlayed.Date == d)
+                        );
+                    }
+                );
+                series.ChartType = SeriesChartType.StackedColumn100;
+                series.ChartArea = chartArea;
+                series.Name = map;
+                series.Legend = legend;
+                series.XValueType = ChartValueType.DateTime;
+            });
+            chart.Visible = true;
+            chart.Size = new Size(600, 300);
+            chart.TabIndex = 0;
+            chart.Location = new Point(0, 70);
+            Axis x = chart.ChartAreas[chartArea].AxisX;
+            x.LabelStyle.Format = "MM-dd";
+            x.Interval = 1;
+            x.IntervalType = DateTimeIntervalType.Days;
+            x.IntervalOffset = 1;
+            
+            Controls.Add(chart);
         }
 
         private void installButton_Click(object sender, EventArgs e)
